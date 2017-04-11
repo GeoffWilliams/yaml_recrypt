08744ad23c540ad851e6d8e51140cfdcd8369c89
Geoff Williams <geoff@geoffwilliams.me.uk>
2017-04-12 00:48:21 +1000

    better debug msg

--- lib/yaml_recrypt.rb
+++ lib/yaml_recrypt.rb
@@ -2,22 +2,38 @@ require "yaml_recrypt/version"
 require 'find'
 require 'escort'
 require 'yaml'
+require 'fileutils'
 
 module YamlRecrypt
-  GPG_MAGIC = "-----BEGIN PGP MESSAGE-----"
+  GPG_MAGIC   = "-----BEGIN PGP MESSAGE-----"
+  BACKUP_EXT  = "orig"
 
   def self.recrypt_file(filename, gpg_private_key, eyaml_pub_key)
     Escort::Logger.output.puts "Processing #{filename}"
 
     # load the yaml into a hash
-    hash = YAML.load(File.readlines(filename).join("\n"))
-    puts "data loaded!"
+
+    # I'm going to hell for this
+    hash_orig = YAML.load(File.readlines(filename).join("\n"))
+    hash_wip  = YAML.load(File.readlines(filename).join("\n"))
 
     # descend every key until a string (or terminal) is reached
-    converted = descend(gpg_private_key, eyaml_pub_key, hash)
+    #converted =
+    descend(gpg_private_key, eyaml_pub_key, hash_wip)
     require 'pp'
 
-    PP.pp(converted)
+    #PP.pp(converted)
+    #PP.pp(hash)
+
+    if hash_orig != hash_wip
+      Escort::Logger.output.puts "***updated values in #{filename}****"
+      # save old file with .orig -- some fool might run this on a non-version
+      # controlled directory...
+      FileUtils.cp(filename, "#{filename}.#{BACKUP_EXT}")
+
+      # save the new data
+      File.open(filename, 'w') {|f| f.write hash_wip.to_yaml }
+    end
   end
 
   def self.descend(gpg_private_key, eyaml_pub_key, value)
@@ -39,7 +55,7 @@ module YamlRecrypt
   def self.process_value(value, gpg_private_key, eyaml_pub_key)
     split = value.split("\n")
 
-    # PGP values are always broken onto newlines using '|'
+    # PGP values are always broken onto newlines
     if split[0].strip == '' and split[1].strip == GPG_MAGIC
       value = recrypt(value, gpg_private_key, eyaml_pub_key)
     end
